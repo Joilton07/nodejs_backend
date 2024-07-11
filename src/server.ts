a88830c8cbb60e87ae795b47d66a857cbde3f1d7
Joilton Alvino <joiltonEstTrab@outlook.com>
2024-07-11 10:50:29 -0300

    Created activity routes and links

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
-import fastify from "fastify"
-import cors from "@fastify/cors"
+import cors from "@fastify/cors";
+import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { prisma } from "./lib/prisma";
-
-import { createTrip } from "./routes/create-trip"
+import { confirmParticipants } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
+import { createActivity } from "./routes/create-activities";
+import { createLink } from "./routes/create-link";
+import { createTrip } from "./routes/create-trip";
+import { getActivities } from "./routes/get-activities";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify()
 
@@ -17,6 +21,11 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipants)
+app.register(createActivity)
+app.register(getActivities)
+app.register(createLink)
+app.register(getLinks)
 
 app.get('/listar', async () => {
   const trips = await prisma.trip.findMany()
