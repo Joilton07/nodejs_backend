31fb15ff69460ca15c5b970a76eabe8799c6847c
Joilton Alvino <joiltonEstTrab@outlook.com>
2024-07-12 22:08:54 -0300

    An adjustment has been made to the error presentations

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import cors from "@fastify/cors";
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { errorHandler } from "./error-handler";
 import { prisma } from "./lib/prisma";
 import { confirmParticipants } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
 import { createActivity } from "./routes/create-activities";
+import { createInvite } from "./routes/create-invite";
 import { createLink } from "./routes/create-link";
 import { createTrip } from "./routes/create-trip";
 import { getActivities } from "./routes/get-activities";
 import { getLinks } from "./routes/get-links";
+import { getParticipant } from "./routes/get-participant";
+import { getParticipants } from "./routes/get-participants";
+import { getTripDetails } from "./routes/get-trip-details";
+import { updateTrip } from "./routes/update-trip";
+import { env } from "./env";
 
 const app = fastify()
 
@@ -19,6 +26,8 @@ app.register(cors, {
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+app.setErrorHandler(errorHandler)
+
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
@@ -26,6 +35,11 @@ app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
 app.get('/listar', async () => {
   const trips = await prisma.trip.findMany()
@@ -33,6 +47,6 @@ app.get('/listar', async () => {
   return trips
 })
 
-app.listen({port: 3333}).then(() => {
+app.listen({port: env.PORT}).then(() => {
   console.log("Server running!")
 })
